2e3ec41113a4e186e5b71c237da172f7ef2e0b80
mafedelahoz <m.delah@uniandes.edu.co>
2024-03-15 08:58:16 -0500

    cambio variable

--- variables/logic/variable_logic.py
+++ variables/logic/variable_logic.py
@@ -4,7 +4,7 @@ def get_variables():
     queryset = Variable.objects.all()
     return (queryset)
 
-def create_variable(form):
-    measurement = form.save()
+def create_variable(str):
+    measurement = str.save()
     measurement.save()
     return ()
